d789b757becd36cba940f576cd725b9682bfed1d
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-02-28 12:03:52 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (93.63%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> ans;
+        //Time Complexity : O(n^2)
+        //Space Complexity :O(n) ignoring the ans vector
         
         for(int i=0;i<nums.size();i++)
         {
             string s="";
+            // first loop is used to store the starting element
             s+=to_string(nums[i]);
             for(int j=i+1;j<nums.size();j++)
             {
+                // if they are not successive elements 
                 if(nums[j-1]+1!=nums[j])
                 {
-                    cout<<j<<" "<<nums[j-1]<<" "<<nums[j]<<"\n";
-                    cout<<"gaya\n";
+                    //if they are equal then it is already stored in s and we have to break
                     if(nums[i]==nums[j-1])
                     {
                         i=j-1;
                         break;
                     }
+                    //it means the index is not stored and we have to store it and it is the final element
                     else
                     {
                         s+="->";
@@ -26,12 +30,12 @@ public:
                         break;
                     }
                 }
+                //it used to check the last elemnt is it successive then we have to store it also
                 if(j==nums.size()-1)
                 {
-                   s+="->";
+                        s+="->";
                         s+=to_string(nums[j]);
                         i=j;
-                        break; 
                 }
             }
             ans.push_back(s);
