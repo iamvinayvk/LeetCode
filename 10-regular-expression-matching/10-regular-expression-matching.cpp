4e17e1887a2750f943f39117dfb76202fe57511e
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-02 15:26:12 +0530

    Time: 7 ms (66.53%), Space: 6.1 MB (98.50%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        bool dp[m+1][n+1];
+        // dp[0][0]=true;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0&&j==0){
+                    dp[i][j]=true;
+                }
+                else if(i==0){
+                    dp[i][j]=false;
+                }
+                else if(j==0)
+                {
+                    if(p[i-1]=='*')
+                    {
+                        dp[i][j]=dp[i-2][j];
+                    }
+                    else
+                    {
+                        dp[i][j]=false;
+                    }
+                }
+                else{
+                     if(p[i-1]==s[j-1]||p[i-1]=='.')
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(p[i-1]=='*'){
+                    dp[i][j]=dp[i-2][j];
+                    if(p[i-2]==s[j-1]||p[i-2]=='.'){
+                        dp[i][j]=dp[i][j]||dp[i][j-1];
+                    }
+                    // else{
+                    //     dp[i][j]=false;
+                    // }
+                }
+                    else {
+                        dp[i][j]=false;
+                    }
+                }
+            }
+        }
+       
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++){
+               
+               
+            }
+        }
+        return dp[m][n];
+    }
+};
