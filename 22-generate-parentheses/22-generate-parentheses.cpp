ef64041cd747901d5701bd16f122b1505f44e1af
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-17 10:32:58 +0530

    Time: 12 ms (21.61%), Space: 13 MB (69.25%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,44 +2,18 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string curr;
-        generateAll(curr,0,0,ans,n);
+        if(n==0){
+            ans.push_back("");
+        }
+        for(int c=0;c<n;c++){
+            for(string left:generateParenthesis(c)){
+                for(string right:generateParenthesis(n-1-c)){
+                    ans.push_back("("+left+")"+right);
+                }
+            }
+        }
         return ans;
         
     }
-    void generateAll(string curr,int open,int close,vector<string>& ans,int n){
-        if(2*n==curr.length()){
-            if(isValid(curr)){
-                ans.push_back(curr);
-                return ;
-            }
-        }
-        if(open<n){
-          curr+='(';
-            generateAll(curr,open+1,close,ans,n); 
-            curr.erase(curr.end()-1);
-        }
-        if(close<open){
-           curr+=')';
-            generateAll(curr,open,close+1,ans,n); 
-            curr.erase(curr.end()-1);
-        }
-            
-            
-        
-    }
-    bool isValid(string curr){
-        int val=0;
-        for(auto c:curr){
-            if(c=='('){
-                val++;
-            }
-            else{
-                val--;
-            }
-            if(val<0)
-                return false;
-        }
-        return val==0;
-    }
+
 };
