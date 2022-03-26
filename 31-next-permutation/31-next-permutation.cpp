a4dc4d5ed3bf5144e7e6b97f46d68eefd18597ad
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-26 11:30:07 +0530

    Time: 7 ms (52.17%), Space: 12.2 MB (37.01%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+      int compareValue=nums[nums.size()-1];
+        int i;
+      for(i=nums.size()-2;i>=0;i--){
+          if(compareValue>nums[i]){
+              compareValue=nums[i];
+              break;
+          }
+          compareValue=nums[i];
+      }  
+        int justGreater=INT_MAX;
+        int j;
+        int index=nums.size();
+        for(j=i+1;j<nums.size();j++){
+            if(nums[j]<=justGreater&&justGreater>=compareValue&&nums[j]>compareValue){
+                cout<<"comapre: "<<compareValue<<" "<<nums[j]<<" "<<justGreater<<"\n";
+                justGreater=nums[j];
+                index=j;
+            }
+        }
+        cout<<i<<" ) "<<j<<" "<<index<<"\n";
+        if(index<nums.size()&&i>=0){
+            swap(nums[i],nums[index]);
+        }
+        reverse(nums.begin()+i+1,nums.end());
     }
 };
