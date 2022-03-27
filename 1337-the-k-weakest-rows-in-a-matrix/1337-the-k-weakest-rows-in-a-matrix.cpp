80a79f331a49caadf83a191cd7c8b2f88fcc2ba7
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-27 18:11:07 +0530

    Time: 21 ms (30.95%), Space: 10.5 MB (75.81%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -4,10 +4,19 @@ public:
         vector<pair<int,int>> store;
         for(int i=0;i<mat.size();i++){
             int count=0;
-            for(int j=0;j<mat[i].size();j++){
-                count+=mat[i][j];
+            int l=0,j=mat[i].size()-1;
+            int index=0;
+            while(l<=j){
+                int mid=l+(j-l)/2;
+                if(mat[i][mid]==1){
+                    l=mid+1;
+                }
+                else{
+                    j=mid-1;
+                }
             }
-            store.push_back({count,i});
+            // cout<<"l: "<<l<<"\n";
+            store.push_back({l,i});
         }
         sort(store.begin(),store.end());
         vector<int> ans;
