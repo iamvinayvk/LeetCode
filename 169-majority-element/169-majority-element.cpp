ec0301a769211970dc6a850e97db5dd261b217ed
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-29 11:51:48 +0530

    Time: 16 ms (92.12%), Space: 19.5 MB (83.59%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int> mp;
+        int count=0,candidate=0;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>nums.size()/2)
-                return nums[i];
+            if(count==0){
+                candidate=nums[i];
+            }
+            count+=(nums[i]==candidate)?1:-1;
         }
-        return -1;
+        return candidate;
     }
 };
