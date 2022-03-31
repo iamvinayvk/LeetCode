3be7e97f4816db842dab1319a1323793dccb4f3d
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-03-31 13:45:58 +0530

    Time: 94 ms (29.64%), Space: 26 MB (67.80%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
+        //Time Complexity : O(n*(log(totalSum)))
+        //Space Complexity: O(1)
         int startSum=1;
         int lastSum=0;
         int ans=0;
@@ -8,15 +10,18 @@ public:
         for(int i=0;i<weights.size();i++){
             lastSum+=weights[i];
         }
+        //binary searched on the total sum search space
         while(startSum<=lastSum){
             int midSum=startSum+(lastSum-startSum)/2;
             if(isPossible(midSum,days,weights)){
                 // cout<<startSum<<" "<<midSum<<" "<<lastSum<<"\n";
+                //if found the sum the reduced the search space from right
                 ans=midSum;
                 lastSum=midSum-1;
             }
             else{
                 // cout<<startSum<<" "<<lastSum<<"\n";
+                //if not found then reduced the search space  from left
                 startSum=midSum+1;
             }
         }
@@ -26,7 +31,10 @@ public:
     bool isPossible(int midSum,int days,vector<int>& weights){
         int requiredDays=1;
         int currWeight=0;
+        // storing the contiguous sum 
         for(int i=0;i<weights.size();i++){
+            //checking if the reuried days is greater than the given days
+            // or if the weight of individual weight is greater then it not possible to ship that weight
             if(weights[i]>midSum||requiredDays>days){
                 // cout<<"midSum: "<<midSum<<" "<<weights[i]<<" "<<requiredDays<<"\n";
                 return false;
