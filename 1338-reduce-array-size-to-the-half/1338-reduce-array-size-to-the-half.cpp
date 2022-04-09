548ddca71b791ae0ccba9db8a19c3dd9746df302
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-04-09 14:40:39 +0530

    Time: 745 ms (5.06%), Space: 78.3 MB (63.03%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(auto x: arr){
+            mp[x]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        int size=0;
+            int ans=0;
+        for(auto x: mp){
+            pq.push({x.second,x.first});
+            size+=x.second;
+            cout<<size<<" \n";
+            if(2*size>arr.size()){
+                cout<<"gaya\n";
+                size-=pq.top().first;
+                ans++;
+                pq.pop();
+                
+            }
+        }
+        return ans;
+        
+        
+    }
+};
