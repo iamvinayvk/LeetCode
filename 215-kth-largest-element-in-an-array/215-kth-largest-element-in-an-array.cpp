ee0c2afa3c88fec7135c961eb8898c5d164f2049
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-04-21 21:48:11 +0530

    Time: 145 ms (8.71%), Space: 9.9 MB (97.70%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,20 +1,39 @@
 class Solution {
 public:
-   
+   int partition(vector<int>& nums,int lo,int hi){
+       int pivot=nums[hi];
+       int i=lo;
+       for(int j=lo;j<=hi;j++){
+           if(nums[j]<=pivot){
+               swap(nums[j],nums[i++]);
+           }
+       }
+       // swap(nums[i-1],nums[hi]);
+       // cout<<i<<" "<<pivot<<"\n";
+       // for(auto x:nums){
+       //     cout<<x<<" ";
+       // }
+       cout<<"\n";
+       return i-1;
+   }
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int i=0;i<nums.size();i++){
-            if(pq.size()==k){
-                if(nums[i]>pq.top())
-                {
-                    pq.pop();
-                    pq.push(nums[i]);
-                }
+        int lo=0,hi=nums.size()-1;
+        k=nums.size()-k;
+        // cout<<"k: "<<k<<"\n";
+        while(lo<=hi){
+            // cout<<"low: "<<lo<<" high: "<<hi<<"\n";
+            int pi=partition(nums,lo,hi);
+            if(pi>k){
+                hi=pi-1;
+            }
+            else if(pi<k){
+                lo=pi+1;
             }
             else{
-                pq.push(nums[i]);
+                return nums[pi];
             }
         }
-        return pq.top();
+        return 0;
+        
     }
 };
