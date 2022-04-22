98077f6593d688e859f8562a4369f3f85e07b61b
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-04-22 16:20:51 +0530

    Time: 381 ms (44.35%), Space: 7.6 MB (44.19%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
-    int findAns(vector<vector<int>>& grid,int n,int m,int r,int c,vector<vector<int>>& visited)     {
-        if(r>=n||c>=m||r<0||c<0||visited[r][c]==1||grid[r][c]==0){
+    int findAns(vector<vector<int>>& grid,int n,int m,int r,int c)     {
+        if(r>=n||c>=m||r<0||c<0||grid[r][c]==0){
             return 0;
         }
-        // visited[r][c]=1;
         int storeGrid=grid[r][c];
         grid[r][c]=0;
-        int down=findAns(grid,n,m,r+1,c,visited);
-        int right=findAns(grid,n,m,r,c+1,visited);
-        int up=findAns(grid,n,m,r-1,c,visited);
-        int left=findAns(grid,n,m,r,c-1,visited);
-        // visited[r][c]=0;
+        int down=findAns(grid,n,m,r+1,c);
+        int right=findAns(grid,n,m,r,c+1);
+        int up=findAns(grid,n,m,r-1,c);
+        int left=findAns(grid,n,m,r,c-1);
         grid[r][c]=storeGrid;
         return grid[r][c]+max({down,right,up,left});
     }
     int getMaximumGold(vector<vector<int>>& grid) {
         int n=grid.size();
         int m=grid[0].size();
-                            vector<vector<int>> visited(n,vector<int>(m,0));
-
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]>0)
                 {
-                    ans=max(findAns(grid,n,m,i,j,visited),ans);    
+                    ans=max(findAns(grid,n,m,i,j),ans);    
                 }
                 
             }
