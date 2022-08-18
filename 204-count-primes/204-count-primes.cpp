6fb3ea35a1eaf468d903ed7b60afc6e3c2bc8376
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-08-18 14:24:32 +0530

    Time: 194 ms (93.62%), Space: 10.3 MB (33.98%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int countPrimes(int n) {
+        int ans=0;
+        vector<bool> isPrime(n+1,false);
+            if(n>2)
+                ans++;
+            
+      for(long long i=3;i<n;i+=2){
+          if(!isPrime[i]){
+              ans++;
+            
+    for(long long  j=i*i;j<=n;j+=i){
+                  isPrime[j]=true;
+              }
+              
+
+          }
+      }
+        return ans;
+    }
+};
