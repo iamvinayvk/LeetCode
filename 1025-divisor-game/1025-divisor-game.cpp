33f5d2e70eb8ecd44cb20b02e76e10c77729bd29
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-08-23 23:16:51 +0530

    Time: 16 ms (5.77%), Space: 6.4 MB (6.99%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        return n%2==0;
+        vector<int> dp(n+1);
+    
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(i%j==0){
+                    dp[i]=dp[i]|(dp[i-j]==0?1:0);
+                }
+            }
+        }
+        return dp[n];
     }
 };
