8faba4741657ca8b5058892b5100cf69bcc46a0a
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-08-28 01:11:45 +0530

    Time: 2302 ms (5.01%), Space: 283 MB (13.89%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,50 +1,37 @@
 class Solution {
 public:
+    bool ans(string& s,int i,int j,vector<vector<int>>& dp){
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        int st=i,en=j;
+        while(st<en){
+            if(s[st++]!=s[en--])
+                return dp[i][j]=false;
+        }
+        return dp[i][j]=true;
+    }
     string longestPalindrome(string s) {
-        vector<vector<int>> dp(s.size()+1,vector<int>(s.size()+1));
-        //for single character setting it as true for palindrome
-        pair<int,int> ans;
-        for(int i=0;i<s.size();i++){
-            dp[i][i]=1;
-            ans.first=i;
-            ans.second=i;
-        }
-        // for two length string 
-        for(int i=0;i<s.size()-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=1;
-                ans.first=i;
-                ans.second=i+1;
-            }
-            else{
-                dp[i][i+1]=0;
+        string res="";
+        int start=0,end=0;
+        int n=s.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        for(int i=0;i<n-1;i++)
+            dp[i][i]=1,dp[i][i+1]=s[i]==s[i+1];
+        dp[n-1][n-1]=1;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                
+                bool made=ans(s,i,j,dp);
+                if(made&&(j-i>=end-start)){
+                    start=i,end=j;
+                }  
             }
         }
-        // now checking if the the previous string i+1 and j-1 are equal or not 
-        int k=2;
-        
-        while(k<s.size()){
-            int i=0;
-            int j=i+k;
-            for(;j<s.size();j++){
-                if(s[i]==s[j]&&dp[i+1][j-1]==1){
-                    dp[i][j]=1;
-                    ans.first=i;
-                    ans.second=j;
-                }
-                else{
-                    dp[i][j]=0;
-                }
-                i++;
-            }
-            k++;
-            
-        }
-        string res;
-        for(int i=ans.first;i<=ans.second;i++)
-        {
-            res+=s[i];
-        }
+        for(int i=start;i<=end;i++)
+            res.push_back(s[i]);
+        // for(int i=start;i<=end;i++)
+        //         res+=s[i];
         return res;
+        
     }
 };
