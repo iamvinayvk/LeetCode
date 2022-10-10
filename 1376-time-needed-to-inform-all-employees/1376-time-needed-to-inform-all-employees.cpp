72456553f36a60f21e78b10968220ef2d1cb4406
Vinay Kushwaha <51132814+iamvinayvk@users.noreply.github.com>
2022-10-10 23:28:38 +0530

    Time: 843 ms (22.84%), Space: 124.6 MB (46.05%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>> graph(n+1);
+        for(int i=0;i<manager.size();i++){
+            int x=manager[i];
+            int y=i;
+            if(x!=-1)
+            graph[x].push_back(y);
+        }
+        queue<int> q;
+        q.push(headID);
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            for(auto child:graph[curr]){
+                informTime[child]+=informTime[curr];
+                q.push(child);
+            }
+        }
+        int ans=0;
+        for(auto x:informTime){
+            ans=max(x,ans);
+        }
+        return ans;
+    }
+};
